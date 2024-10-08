b200d8e61905b7876982541111ca3a80760a4a91
km-zen <konrad.markowski.1993@gmail.com>
2024-10-08 18:03:16 +0200

    implemented solution for task02 v.2

--- task02/jsrc/main/java/com/task02/HelloWorldLambda.java
+++ task02/jsrc/main/java/com/task02/HelloWorldLambda.java
@@ -6,7 +6,14 @@ import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,13 +26,30 @@ import java.util.Map;
 @LambdaUrlConfig(
 		authType = AuthType.AWS_IAM
 )
-public class HelloWorldLambda implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorldLambda implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+	@Override
+	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+		if ("GET".equals(event.getRequestContext().getHttp().getMethod()) && "/hello".equals(event.getRawPath())) {
+			return buildResponse(200, "Hello from lambda");
+		} else {
+			return buildResponse(404, "Not Found");
+		}
+	}
+
+	private APIGatewayV2HTTPResponse buildResponse(int statusCode, String message) {
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Content-Type", "application/json");
+
+		Map<String, Object> responseBody = new HashMap<>();
+		responseBody.put("message", message);
+
+		return APIGatewayV2HTTPResponse.builder()
+				.withStatusCode(statusCode)
+				.withHeaders(headers)
+				.withBody(gson.toJson(responseBody))
+				.build();
 	}
 }

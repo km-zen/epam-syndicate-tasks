6850ce5a580be2e0bb823057e493ca2834a2d806
km-zen <konrad.markowski.1993@gmail.com>
2024-10-11 20:42:36 +0200

    implemented task04 v.1 SQS part added

---
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,50 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(
+    lambdaName = "sqs_handler",
+	roleName = "sqs_handler-role",
+	isPublishVersion = false,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SqsTriggerEventSource(
+		targetQueue = "async_queue",
+		batchSize = 1
+)
+@DependsOn(
+		name = "async_queue",
+		resourceType = ResourceType.SQS_QUEUE
+
+)
+public class SqsHandler implements RequestHandler<SQSEvent, Void> {
+
+	public Void handleRequest(SQSEvent sqsEvent, Context context){
+		for(SQSEvent.SQSMessage msg : sqsEvent.getRecords()){
+			processMessage(msg, context);
+		}
+		context.getLogger().log("done");
+		return null;
+	}
+
+	private void processMessage(SQSEvent.SQSMessage msg, Context context){
+		try{
+			context.getLogger().log("Processed message " + msg.getBody());
+		} catch (Exception e){
+			context.getLogger().log("An error occurred");
+			throw e;
+		}
+	}
+
+}

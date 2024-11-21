d360474492748a8084ebf28c1a783dd3a6f4c103
km-zen <konrad.markowski.1993@gmail.com>
2024-11-21 09:02:05 +0100

    implemented solution v26

--- task10/jsrc/main/java/com/task10/handler/GetTablesHandler.java
+++ task10/jsrc/main/java/com/task10/handler/GetTablesHandler.java
@@ -1,9 +1,7 @@
 package com.task10.handler;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
-
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
@@ -11,10 +9,11 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-
 import org.json.JSONObject;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetTablesHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
@@ -43,7 +42,8 @@ public class GetTablesHandler implements RequestHandler<APIGatewayProxyRequestEv
         }
 
         return new APIGatewayProxyResponseEvent()
-                .withBody(tempList.toString());
+                .withStatusCode(200)
+                .withBody(new JSONObject().put("tables", tempList).toString());
     }
 
 }
